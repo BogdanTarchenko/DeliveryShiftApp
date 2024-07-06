d271869772798264976c5c339c807dbecd20afb7
BogdanTarchenko <tarchenko.bogdan@outlook.com>
2024-07-06 23:05:37 +0600

    Created calculation screen, need to fix UI bugs

--- DeliveryShiftApp/Application/SceneDelegate.swift
+++ DeliveryShiftApp/Application/SceneDelegate.swift
@@ -14,10 +14,30 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        
+        let firstVC = ViewController()
+        firstVC.tabBarItem = UITabBarItem(title: "Расчёт", image: UIImage(named: "calculate.pdf"), tag: 0)
+                
+        let secondVC = SecondViewController()
+        secondVC.tabBarItem = UITabBarItem(title: "История", image: UIImage(named: "time.pdf"), tag: 1)
+                
+        let thirdVC = ThirdViewController()
+        thirdVC.tabBarItem = UITabBarItem(title: "Профиль", image: UIImage(named: "user.pdf"), tag: 2)
+        
+        let tabBarVC = UITabBarController()
+        tabBarVC.setViewControllers([firstVC, secondVC, thirdVC], animated: true)
+        tabBarVC.selectedIndex = 0
+        
+        window.rootViewController = tabBarVC
+        window.backgroundColor = .white
+        
+        secondVC.loadViewIfNeeded()
+        thirdVC.loadViewIfNeeded()
+        
         self.window = window
-        self.window?.makeKeyAndVisible()
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -50,4 +70,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
+
 
